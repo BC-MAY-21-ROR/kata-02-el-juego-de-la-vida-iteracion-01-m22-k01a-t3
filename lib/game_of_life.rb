24513d2a798aafe75e884860a3e1a4b9cab7d383
EdwardMD <Edwardmi444@gmail.com>
2022-05-10 17:34:34 -0500

    Refactor simple uwu
    Co-authors: @lucasvidela94
    Co-authors: @d1eg0dev
    Co-authors: @guillermo-delprado

--- lib/game_of_life.rb
+++ lib/game_of_life.rb
@@ -1,73 +1,14 @@
-class Game
-
-    LIVE = "*"
-    DEAD = "."
-    puts "Elegir ancho del tablero"
-    WIDTH = gets.chomp.to_i
-    puts "Elegir alto del tablero"
-    HEIGHT = gets.chomp.to_i
-    
-    #Funcion que define que estado de celula
-    def init
-        #Nuevo grid vacío 
-        @grid = empty_grid
-
-        @grid.each do |row|
-            row.map! do |cell|
-                if rand(5) == 1
-                    LIVE #celula viva
-                else
-                    DEAD #celula muerta
-                end
-            end
+puts "Elegir ancho del tablero"
+$width = gets.chomp.to_i
+puts "Elegir alto del tablero"
+$height = gets.chomp.to_i
+def init
+    $matriz = Array.new($width){Array.new($height){["*","."].sample}}
+    $width.times do |i|
+        $height.times do |j|
+            print $matriz[i][j]
         end
-        
+        puts
     end
-
-    #Funcion que genera tablero con celulas muertas
-    def empty_grid
-        Array.new(HEIGHT){ Array.new(WIDTH) {DEAD} }
-    end
-
-    def print_grid
-        # system "cls"
-
-        rows = @grid.map do |row|
-            row.join(" ")
-        end
-
-        print rows.join("\n")
-    end
-
-    def prueba 
-        print_grid
-        new_grid = empty_grid
-
-        @grid.each_with_index do |row,row_index|
-            row.each_with_index do |cell,cell_index|
-                
-                neighbors = find_neighbors(row_index,cell_index)
-
-                if cell = LIVE
-                    state = neighbors.size.between?(2,3)
-                else
-                    state = neighbors.size == 3
-                end
-
-                new_grid[row_index][cell_index] = state? LIVE : DEAD
-
-            end
-
-        end
-
-        @grid = new_grid
-        reinicio
-    end
-
-
-
-    
- 
 end
-
-Game.new
+init
